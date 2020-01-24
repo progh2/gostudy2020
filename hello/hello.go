b34a79cbae2c8ad11fe796551bbe117d782d3449
Gihun Ham <progh2@gmail.com>
2020-01-25 01:24:00 +0900

    2.1 switch문 case 조건식 사용

--- hello/hello.go
+++ hello/hello.go
@@ -24,12 +24,22 @@ func main() {
 
 	sum3, i3 := 0, 0
 	// for 문에 조건식 생략
-	for{
-		if i3>= 10 {
+	for {
+		if i3 >= 10 {
 			break
 		}
 		sum3 += i3
 		i3++
 	}
 	fmt.Println(sum3)
+
+	c := 'a'
+	switch {
+	case '0' <= c && c <= '9':
+		fmt.Printf("%c은(는) 숫자입니다", c)
+	case 'a' <= c && c <= 'z':
+		fmt.Printf("%c은(는) 소문자입니다", c)
+	case 'A' <= c && c <= 'Z':
+		fmt.Printf("%c은(는) 대문자입니다", c)
+	}
 }

0c97c42fd219182eca6e9e3dea189c33b200338f
Gihun Ham <progh2@gmail.com>
2020-01-25 01:20:22 +0900

    2.1 for 반복문1

--- hello/hello.go
+++ hello/hello.go
@@ -5,4 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("안녕, 세상아?!")
+
+	// 처음 정의 및 할당 할 때는 := 사용. 그 후 할당 때는 = 사용.
+	sum := 0
+	// for 초기화 조건; 조건식; 후속작업 정의
+	// go에는 while이 없다.
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
 }
